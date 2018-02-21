e1f240c16a73634a8c3e229d37e490bff5dcff96
Juan Tenezaca <tenj34@gmail.com>
2018-02-21 12:34:44 -0500

    PH1 | added the map pack and ability to center at location

--- PokeHunter/ViewController.swift
+++ PokeHunter/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var updateCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            print ("We are ready to use")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      
+        if updateCount < 3 {
+            let region  = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 400, 400)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
     }
-
-
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate{
+        
+        let region  = MKCoordinateRegionMakeWithDistance(coord, 400, 400)
+        mapView.setRegion(region, animated: true)
+        updateCount += 1
+      }
+    }
 }
 
